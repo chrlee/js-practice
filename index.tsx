dd6acb6cb8560ea5362aa750e02ce0adc41c3d7b
Christian Lee <cllz@amazon.com>
2024-12-04 04:48:06 -0500

    React scaffold

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import * as ReactDOM from "react-dom/client";
+import App from "./src/App";
+
+const root = ReactDOM.createRoot(document.getElementById('app') as Element);
+
+root.render(<App />);
---
+++ webpack.config.cjs
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './index.tsx',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+  ],
+};
